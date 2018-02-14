daf00c186659717a5160dee3062f379da33bbfbe
Hyeok Jung Kim <hyeokjungkim@gmail.com>
2018-02-14 19:19:12 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,8 +7,11 @@ def batch_badge_creator(array)
   array.map {|name| badge_maker(name)}
 end
 
-def assign_rooms(name)
-
+def assign_rooms(array)
+  arr = []
+  for i in 0..array.size do
+    arr < "Hello, #{array[i]}! You'll be assigned to #{i+1}!"
+  end
 end
 
 def printer
